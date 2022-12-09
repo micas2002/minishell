a805d6576c7ba2f7c79f6a35aa7a4b240c9d1630
micas2002 <miguelbernardinosousa2002@gmail.com>
2022-12-09 14:12:06 +0000

    metachracters recognition is not working properly

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,17 +6,35 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:49:49 by mibernar          #+#    #+#             */
-/*   Updated: 2022/12/09 12:52:02 by mibernar         ###   ########.fr       */
+/*   Updated: 2022/12/09 13:54:52 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	is_operator(char c)
+{
+	if (c == '|' || c == '&' || c == ';' || c == '(' || c == ')' || c == '<'
+		|| c == '>')
+		return (1);
+	return (0);
+}
+
+int	operators(char *str, int i)
+{
+	while (str[++i])
+	{
+		if (is_operator(str[i] == 0))
+			break ;
+	}
+	return (i);
+}
+
 int	other_input(char *str, int i)
 {
 	while (str[++i])
 	{
-		if (str[i] == ' ' || str[i] == '	')
+		if (str[i] == ' ' || str[i] == '	' || is_operator(str[i]) == 1)
 			break ;
 	}
 	return (i);
@@ -42,14 +60,6 @@ int	s_quotes(char *str, int i)
 	return (i);
 }
 
-int	is_operator(char c)
-{
-	if (c == '|' || c == '&' || c == ';' || c == '(' || c == ')' || c == '<'
-		|| c == '>')
-		return (1);
-	return (0);
-}
-
 void	lexer(char *str, t_minish minish)
 {
 	int		i;
@@ -67,11 +77,14 @@ void	lexer(char *str, t_minish minish)
 				i = d_quotes(str, i);
 			if (str[i] == '\'')
 				i = s_quotes(str, i);
+			if (is_operator(str[i]) == 1)
+				i = operators(str, i);
 			else
 				i = other_input(str, i);
 			minish.nb_tokens++;
 		}
 	}
+	printf("nb of tokens:%d\n", minish.nb_tokens);
 	minish.tokens = malloc(sizeof(char *) * minish.nb_tokens + 1);
 	i = 0;
 	x = 0;
@@ -87,6 +100,8 @@ void	lexer(char *str, t_minish minish)
 				i = d_quotes(str, i);
 			if (str[i] == '\'')
 				i = s_quotes(str, i);
+			if (is_operator(str[i]) == 1)
+				i = operators(str, i);
 			else
 				i = other_input(str, i);
 			minish.tokens[++y] = ft_substr(str, x, i - x);
