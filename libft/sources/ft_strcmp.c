b4558fcbd95885a4351b4220f7266eb49ad1a83c
CanIBeFii <filipe250502@gmail.com>
2023-02-15 19:14:30 +0000

    changed ft_strcmp to return the diff of both str

--- libft/sources/ft_strcmp.c
+++ libft/sources/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miguel <miguel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 12:40:02 by miguel            #+#    #+#             */
-/*   Updated: 2022/06/22 12:45:28 by miguel           ###   ########.fr       */
+/*   Updated: 2023/02/15 19:13:43 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 		&& ((unsigned char)s1[x] != '\0'
 			&& (unsigned char)s2[x] != '\0'))
 		x++;
-	if ((unsigned char)s1[x] != (unsigned char)s2[x])
-		return (0);
-	return (1);
+	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
 }
