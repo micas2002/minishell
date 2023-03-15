9d46cb97eb60a1a77056419619fc9700cc7292a5
CanIBeFii <filipe250502@gmail.com>
2023-03-15 15:13:52 +0000

    fix: move functions to utils file
    parser now saves the ammount of different commands given

--- sources/input.c
+++ sources/input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 15:01:08 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/14 16:19:14 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/15 15:13:26 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,87 +47,7 @@ int	parser(char *input, t_shell *shell)
 	if (quote_checker(input) == 0)
 		return (error_handler(ERR_UNCLOSED_QUOTES, 1));
 	commands = ft_split(input, '|');
+	shell->num_tokens = get_double_array_size(commands);
 	shell->tokens = divide_tokens(&commands);
 	return (0);
-}
-
-t_token	*create_token(char **str)
-{
-	t_token	*token;
-	int		iter;
-	int		command;
-
-	token = get_redirection(str, 0, -1);
-	iter = 0;
-	command = 0;
-	while (str[iter] != NULL)
-		iter++;
-	token->arguments = malloc(sizeof(char *)
-			* (iter - (token->num_files * 2) + 1));
-	iter = 0;
-	while (str[iter] != NULL)
-	{
-		if (is_redir(str[iter]) == 1 && str[iter + 1] != NULL)
-			iter += 2;
-		else if (is_redir(str[iter]) == 1 && str[iter + 1] == NULL)
-			return (NULL);
-		else
-		{
-			if (command == 0)
-				token->program_name = ft_strdup(str[iter]);
-			token->arguments[command] = ft_strdup(str[iter]);
-			command++;
-			iter++;
-		}
-	}
-	return (token);
-}
-
-t_token	*get_redirection(char **str, int count, int iter)
-{
-	t_token	*token;
-
-	token = malloc(sizeof(t_token));
-	while (str[++iter] != NULL)
-	{
-		if (is_redir(str[iter]) == 1)
-			count++;
-	}
-	token->redirections = malloc(sizeof(char *) * (count + 1));
-	token->files = malloc(sizeof(char *) * (count + 1));
-	token->num_redirections = count;
-	token->num_files = count;
-	count = 0;
-	iter = -1;
-	while (str[++iter] != NULL)
-	{
-		if (is_redir(str[iter]) == 1)
-		{
-			token->redirections[count] = str[iter];
-			token->files[count] = str[iter + 1];
-			count++;
-		}
-	}
-	token->redirections[count] = NULL;
-	token->files[count] = NULL;
-	return (token);
-}
-
-t_token	**divide_tokens(char ***array)
-{
-	int		iter;
-	t_token	**tokens;
-
-	iter = 0;
-	while (array[iter] != NULL)
-		iter++;
-	tokens = malloc(sizeof(t_token) * (iter + 1));
-	iter = 0;
-	while (array[iter] != NULL)
-	{
-		tokens[iter] = create_token(array[iter]);
-		iter++;
-	}
-	tokens[iter] = NULL;
-	return (tokens);
 }
--- sources/input_utils.c
+++ sources/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:27:33 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/15 14:44:33 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/03/15 15:11:55 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,85 @@ char	**minishell_split(char const *s, char c, int i)
 		return (NULL);
 	str = minishell_split_loop(s, str, 0, 0);
 	return (minishell_split_loop(s, str, 0, 0));
+}
+
+t_token	**divide_tokens(char ***array)
+{
+	int		iter;
+	t_token	**tokens;
+
+	iter = 0;
+	while (array[iter] != NULL)
+		iter++;
+	tokens = malloc(sizeof(t_token) * (iter + 1));
+	iter = 0;
+	while (array[iter] != NULL)
+	{
+		tokens[iter] = create_token(array[iter]);
+		iter++;
+	}
+	tokens[iter] = NULL;
+	return (tokens);
+}
+
+t_token	*create_token(char **str)
+{
+	t_token	*token;
+	int		iter;
+	int		command;
+
+	token = get_redirection(str, 0, -1);
+	iter = 0;
+	command = 0;
+	while (str[iter] != NULL)
+		iter++;
+	token->arguments = malloc(sizeof(char *)
+			* (iter - (token->num_files * 2) + 1));
+	iter = 0;
+	while (str[iter] != NULL)
+	{
+		if (is_redir(str[iter]) == 1 && str[iter + 1] != NULL)
+			iter += 2;
+		else if (is_redir(str[iter]) == 1 && str[iter + 1] == NULL)
+			return (NULL);
+		else
+		{
+			if (command == 0)
+				token->program_name = ft_strdup(str[iter]);
+			token->arguments[command] = ft_strdup(str[iter]);
+			command++;
+			iter++;
+		}
+	}
+	return (token);
+}
+
+t_token	*get_redirection(char **str, int count, int iter)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	while (str[++iter] != NULL)
+	{
+		if (is_redir(str[iter]) == 1)
+			count++;
+	}
+	token->redirections = malloc(sizeof(char *) * (count + 1));
+	token->files = malloc(sizeof(char *) * (count + 1));
+	token->num_redirections = count;
+	token->num_files = count;
+	count = 0;
+	iter = -1;
+	while (str[++iter] != NULL)
+	{
+		if (is_redir(str[iter]) == 1)
+		{
+			token->redirections[count] = str[iter];
+			token->files[count] = str[iter + 1];
+			count++;
+		}
+	}
+	token->redirections[count] = NULL;
+	token->files[count] = NULL;
+	return (token);
 }
