a706944bc5b1ff938877b32a11ac4ef3840b583a
CanIBeFii <filipe250502@gmail.com>
2023-03-15 17:58:45 +0000

    fix(input_utils): fix file corruption
    Co-authored-by: micas2002 miguelbernardinosousa2002@gmail.com

--- sources/input_utils.c
+++ sources/input_utils.c
@@ -3,13 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   input_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:27:33 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/15 17:42:17 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/15 17:58:22 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+t_token **divide_tokens(char **cmds)
 {
 	t_token	**tokens;
 	int		iter;
@@ -23,11 +26,18 @@
 	{
 		tokens[iter] = (t_token *)malloc(sizeof(t_token));
 		tokens[iter]->args = lexer(cmds[iter], ' ');
+		tokens[iter]->str = ft_strdup(cmds[iter]);
 		iter++;
 	}
 	return (tokens);
 }
 
-		tokenstokens[iter]->cmd = 		iter+to		tokens[iter]->next = NULL
-	tke	tokens[iter] = NULL;
-strft_str		tokens[iter]->str = ft_strdup(cmds[iter]);
+int	get_array_size(char **str)
+{
+	int	iter;
+
+	iter = 0;
+	while (str[iter] != NULL)
+		iter++;
+	return (iter);	
+}
