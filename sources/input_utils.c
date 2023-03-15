5728932fee820b06fb94168d5c21f04531e4afed
CanIBeFii <filipe250502@gmail.com>
2023-03-15 14:48:05 +0000

    fix: separated functions in input utils
    fix norm errors in function minishell_split

--- sources/input_utils.c
+++ sources/input_utils.c
@@ -3,92 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   input_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:27:33 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/14 16:57:38 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/15 14:44:33 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	quote_handler(char *str, int i, char c)
+char	**minishell_split_loop(char const *s, char **str, int i, int x)
 {
-	while (str[i] != '\0')
-	{
-		if (str[i] == c)
-			return (i);
-		i++;
-	}
-	return (-1);
-}
+	int	begin;
 
-int	quote_checker(char *str)
-{	
-	int	x;
-
-	x = 0;
-	while (str[x] != '\0')
-	{
-		if (str[x] == '\'')
-			x = quote_handler(str, x, '\'');
-		if (str[x] == '\'')
-			x = quote_handler(str, x, '\'');
-		if (x == -1)
-			return (0);
-		x++;
-	}
-	return (1);
-}
-
-int	get_array_size(char *s, char c)
-{
-	int	i;
-	int	control;
-	int	size;
-
-	i = 0;
-	control = 0;
-	size = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == '\'')
-			i = quote_handler(s, i, '\'');
-		if (s[i] == '\"')
-			i = quote_handler(s, i, '\"');
-		if (i == -1)
-			return (-1);
-		if (s[i] != c && control == 0)
-		{
-			control = 1;
-			size++;
-		}
-		else if (s[i] == c)
-			control = 0;
-		i++;
-	}
-	return (size);
-}
-
-char	**minishell_split(char const *s, char c, int i)
-{
-	int		array_size;
-	char	**str;
-	int		x;
-	int		begin;
-
-	if (!s)
-		return (NULL);
-	array_size = get_array_size((char *)s, c);
-	if (array_size == -1)
-		return (NULL);
-	str = (char **)malloc(sizeof(char *) * (array_size + 1));
-	if (!str)
-		return (NULL);
-	x = 0;
 	begin = 0;
-	i = 0;
-	x = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == '\'')
@@ -110,16 +38,19 @@ char	**minishell_split(char const *s, char c, int i)
 	return (str);
 }
 
-int	is_redir(char *str)
+char	**minishell_split(char const *s, char c, int i)
 {
-	if (ft_strcmp(str, ">") == 0)
-		return (1);
-	else if (ft_strcmp(str, "<") == 0)
-		return (1);
-	else if (ft_strcmp(str, ">>") == 0)
-		return (1);
-	else if (ft_strcmp(str, "<<") == 0)
-		return (1);
-	else
-		return (0);
+	int		array_size;
+	char	**str;
+
+	if (!s)
+		return (NULL);
+	array_size = get_array_size((char *)s, c);
+	if (array_size == -1)
+		return (NULL);
+	str = (char **)malloc(sizeof(char *) * (array_size + 1));
+	if (!str)
+		return (NULL);
+	str = minishell_split_loop(s, str, 0, 0);
+	return (minishell_split_loop(s, str, 0, 0));
 }
---
+++ sources/input_utils2.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_utils2.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 14:26:46 by fialexan          #+#    #+#             */
+/*   Updated: 2023/03/15 14:27:38 by fialexan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_redir(char *str)
+{
+	if (ft_strcmp(str, ">") == 0)
+		return (1);
+	else if (ft_strcmp(str, "<") == 0)
+		return (1);
+	else if (ft_strcmp(str, ">>") == 0)
+		return (1);
+	else if (ft_strcmp(str, "<<") == 0)
+		return (1);
+	else
+		return (0);
+}
+
+int	quote_handler(char *str, int i, char c)
+{
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	quote_checker(char *str)
+{	
+	int	x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		if (str[x] == '\'')
+			x = quote_handler(str, x, '\'');
+		if (str[x] == '\'')
+			x = quote_handler(str, x, '\'');
+		if (x == -1)
+			return (0);
+		x++;
+	}
+	return (1);
+}
+
+int	get_array_size(char *s, char c)
+{
+	int	i;
+	int	control;
+	int	size;
+
+	i = 0;
+	control = 0;
+	size = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\'')
+			i = quote_handler(s, i, '\'');
+		if (s[i] == '\"')
+			i = quote_handler(s, i, '\"');
+		if (i == -1)
+			return (-1);
+		if (s[i] != c && control == 0)
+		{
+			control = 1;
+			size++;
+		}
+		else if (s[i] == c)
+			control = 0;
+		i++;
+	}
+	return (size);
+}
