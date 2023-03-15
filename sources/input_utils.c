4b4ab539680eaba2feb7b87761f352df7482fd13
CanIBeFii <filipe250502@gmail.com>
2023-03-15 18:23:41 +0000

    feat: start handle of dollar sign in input

--- sources/input_utils.c
+++ sources/input_utils.c
@@ -6,11 +6,37 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:27:33 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/15 17:58:22 by fialexan         ###   ########.fr       */
+/*   Updated: 2023/03/15 18:22:29 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_token	**handle_dollar(t_token *tokens)
+{
+	int		iter;
+	int		iter2;
+	char	*tmp;
+
+	iter = 0;
+	while (tokens->args[iter] != NULL)
+	{
+		iter2 = 0;
+		while (tokens->args[iter][iter2] != '\0')
+		{
+			if (tokens->args[iter][iter2] == '$')
+			{
+				tmp = ft_strdup(tokens->args[iter]);
+				free(tokens->args[iter]);
+				tokens->args[iter] = ft_strjoin(ft_substr(tmp, 0, iter2), ft_substr(tmp, iter2 + 1, ft_strlen(tmp)));
+				free(tmp);
+			}
+			iter2++;
+		}
+		iter++;
+	}
+	return (tokens->args);
+}
 
 t_token **divide_tokens(char **cmds)
 {
