a57931bb6402d43b85b14b6efed30d9fc2ba255f
CanIBeFii <filipe250502@gmail.com>
2023-03-17 19:35:43 +0000

    fix: fix norm errors
    delete unecessary commented function

--- sources/echo.c
+++ sources/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:51:10 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/17 18:38:49 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/17 19:33:37 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,70 +17,26 @@
 //input, it prints the message. If the -n flag exists, it doesn't print the
 //newline
 
-/*void	echo(t_token *tokens)
+char	*delete_quotes(char	*str, char current_quote, int iter, int str_iter)
 {
-	char	*str;
-	int		x;
-
-	if (!tokens->args[1])
-	{
-		printf("\n");
-		return ;
-	}
-	x = i + 1;
-	if (ft_strcmp(tokens[i]->args[i + 1], "-n") == 0)
-		i++;
-	while (shell->tokens[i] && ft_strcmp(shell->tokens[i], "|") != 0)
-	{
-		if (shell->tokens[i][0] == '\"')
-			str = ft_strtrim(shell->tokens[i], "\"");
-		else
-			str = ft_strtrim(shell->tokens[i], "\'");
-		printf("%s", str);
-		if (shell->tokens[i + 1] && is_operator(shell->tokens[i + 1][0]) == 0)
-			printf(" ");
-		i++;
-		if (str)
-			free(str);
-	}
-	if (ft_strcmp(shell->tokens[x], "-n") == 0)
-		return ;
-	printf("\n");
-} */
-
-char	*delete_quotes(char	*str)
-{
-	char	current_quote;
 	char	*new_str;
-	int		iter;
-	int		str_iter;
-	
+
 	new_str = (char *)ft_calloc(sizeof(char), ft_strlen(str) + 1);
 	if (new_str == NULL)
 		return (NULL);
-	iter = 0;
-	str_iter = 0;
-	current_quote = ' ';
-	while (str[iter] != '\0')
+	while (str[++iter] != '\0')
 	{
 		if ((str[iter] == '\'' || str[iter] == '\"'))
 		{
 			if (str[iter] != current_quote && current_quote != ' ')
-			{
-				new_str[str_iter] = str[iter];
-				str_iter++;
-			}
+				new_str[str_iter++] = str[iter];
 			else if (str[iter] == current_quote && current_quote != ' ')
 				current_quote = ' ';
 			else
 				current_quote = str[iter];
 		}
 		else
-		{
-			new_str[str_iter] = str[iter];
-			str_iter++;
-		}
-		iter++;
+			new_str[str_iter++] = str[iter];
 	}
 	return (new_str);
 }
@@ -88,7 +44,9 @@ char	*delete_quotes(char	*str)
 int	check_option(char *str)
 {
 	int	iter;
-	
+
+	if (str == NULL)
+		return (0);
 	if (str[0] != '-')
 		return (0);
 	iter = 1;
@@ -106,20 +64,15 @@ void	echo(t_token *token)
 	char	*str;
 	int		iter;
 
-	if (token->args[1] == NULL)
-	{
-		printf("\n");
-		return ;
-	}
 	iter = 1;
 	if (check_option(token->args[1]) == 1)
 		iter++;
 	while (token->args[iter] != NULL)
 	{
 		if (ft_srchr(token->args[iter], '\'') != NULL
-				|| ft_strchr(token->args[iter], '\"') != NULL)
+			|| ft_strchr(token->args[iter], '\"') != NULL)
 		{
-			str = delete_quotes(token->args[iter]);
+			str = delete_quotes(token->args[iter], ' ', -1, 0);
 			printf("%s", str);
 			free(str);
 		}
