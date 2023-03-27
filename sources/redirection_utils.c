452c9a353574669ab9eafb078bece177846c28d2
CanIBeFii <filipe250502@gmail.com>
2023-03-27 18:03:12 +0100

    feat: add clean_redirections method

--- sources/redirection_utils.c
+++ sources/redirection_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirection_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:02:47 by filipe            #+#    #+#             */
-/*   Updated: 2023/03/25 13:56:24 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/27 17:35:56 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,50 @@ int	input_redirection(char *redirection, char *filename, int num_redirection)
 	if (num_redirection == 0)
 		return (fd);
 	return (-1);
+}
+
+t_token	*clean_redirections(t_token *token)
+{
+	int		iter;
+	int		count;
+	t_token	*new_token;
+
+	iter = 0;
+	count = 0;
+	while (token->args[iter] != NULL)
+	{
+		if (ft_strcmp(token->args[iter], ">") == 0
+			|| ft_strcmp(token->args[iter], ">>") == 0
+			|| ft_strcmp(token->args[iter], "<") == 0
+			|| ft_strcmp(token->args[iter], "<<") == 0)
+			count += 2;
+		iter++;
+	}
+	new_token = malloc(sizeof(t_token));
+	new_token->args = malloc(sizeof(char *) * (iter - count + 1));
+	new_token = clean_redirections_loop(new_token, token);
+	return (new_token);
+}
+
+t_token	*clean_redirections_loop(t_token *new_token, t_token *token)
+{
+	int	iter;
+	int	count;
+
+	while (token->args[iter] != NULL)
+	{
+		if (ft_strcmp(token->args[iter], ">") == 0
+			|| ft_strcmp(token->args[iter], ">>") == 0
+			|| ft_strcmp(token->args[iter], "<") == 0
+			|| ft_strcmp(token->args[iter], "<<") == 0)
+			iter += 2;
+		else
+		{
+			new_token->ars[count] = ft_strdup(token->args[iter]);
+			iter++;
+			count++;
+		}
+	}
+	new_token->args[count] = NULL;
+	return (new_token);
 }
