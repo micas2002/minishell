28b1c2d1de9f91a0e33440b16dc98fa87bb25bbe
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-03-28 15:44:14 +0100

    fixed leak when spaces where added before and/or after input word

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,7 +6,7 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:49:49 by mibernar          #+#    #+#             */
-/*   Updated: 2023/03/27 17:09:53 by mibernar         ###   ########.fr       */
+/*   Updated: 2023/03/28 15:42:09 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,26 +42,48 @@ char	**lexer_loop(char const *s, char **str, int i, int x)
 	int	begin;
 
 	begin = 0;
-	while (s[i] != '\0')
+	// while (s[i] != '\0')
+	// {
+	// 	if (s[i] == '\'')
+	// 		i = quote_handler((char *)s, i + 1, '\'') + 1;
+	// 	else if (s[i] == '\"')
+	// 		i = quote_handler((char *)s, i + 1, '\"') + 1;
+	// 	else if (s[i] == ' ')
+	// 	{
+	// 		str[x] = ft_substr((char *)s, begin, i - begin);
+	// 		while (s[i] == ' ')
+	// 			i++;
+	// 		begin = i;
+	// 		x++;
+	// 	}
+	// 	else
+	// 		i++;
+	// 	if (i == -1)
+	// 		return (unclosed_quote_found(str, x));
+	// }
+	// str[x] = ft_substr((char *)s, begin, i - begin);
+	// str[++x] = NULL;
+	while (s[i])
 	{
-		if (s[i] == '\'')
-			i = quote_handler((char *)s, i + 1, '\'') + 1;
-		else if (s[i] == '\"')
-			i = quote_handler((char *)s, i + 1, '\"') + 1;
-		else if (s[i] == ' ')
-		{
-			str[x] = ft_substr((char *)s, begin, i - begin);
+		if (s[i] == ' ' || s[i] == '\t')
 			i++;
+		else
+		{
 			begin = i;
+			if (s[i] == '\'')
+				i = quote_handler((char *)s, i + 1, '\'') + 1;
+			else if (s[i] == '\"')
+				i = quote_handler((char *)s, i + 1, '\"') + 1;
+			else
+				while (s[i] && s[i] != ' ')
+					i++;
+			if (i == -1)
+				return (unclosed_quote_found(str, x));
+			str[x] = ft_substr(s, begin, i - begin);
 			x++;
 		}
-		else
-			i++;
-		if (i == -1)
-			return (unclosed_quote_found(str, x));
 	}
-	str[x] = ft_substr((char *)s, begin, i - begin);
-	str[++x] = NULL;
+	str[x] = NULL;
 	return (str);
 }
 
